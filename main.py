e0b014e0076d3357bf6d2b4e526486ccc0c16a87
DaniilSemizhonov <BambooStudios@mail.ru>
2022-02-25 21:11:37 +1000

    1.1

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
-from pywebio.input import input, FLOAT
-from pywebio.output import put_text
-import argparse
-import asyncio
 
 from pywebio import start_server
 from pywebio.input import *
 from pywebio.output import *
-from pywebio.session import defer_call, info as session_info, run_async
+
 
 def main():
     put_markdown("## Решение задачек по информатики ##")
     task_type = select("Какую задачу хочешь решить", ['Расчитать обьём видеопамяти',
     'Узнать размер файла изображения', 'Узнать максимально возможное число цветов в изображении'])
     if task_type == 'Расчитать обьём видеопамяти':
-        data = input_group("Basic info", [
-            input('Input monitor_resolution1', name='monitor_resolution1',type=FLOAT),
-            input('Input monitor_resolution2', name='monitor_resolution2',type=FLOAT),
-            input('Input colors', name='colors',type=FLOAT)
+        data = input_group("Условия задачи", [
+            input('Разрешение монитора первое число', name='monitor_resolution1',type=FLOAT),
+            input('Разрешение монитора второе число', name='monitor_resolution2',type=FLOAT),
+            input('Количество цветов', name='colors',type=FLOAT)
         ])
         monitor_resolution1 = data['monitor_resolution1']
         monitor_resolution2 = data['monitor_resolution2']
@@ -32,27 +28,35 @@ def main():
         result = monitor_result * bit / 8 / 1024
         #print(f"Ответ {result} Кбайт")
         put_text(f"Ответ {result} Кбайт")
-    elif task_type == 2:
-        monitor_resolution1 = int(input("Разрешение экрана 1 число "))
-        monitor_resolution2 = int(input("Разрешение экрана 2 число "))
+    elif task_type == "Узнать размер файла изображения":
+        data = input_group("Условия задачи", [
+            input('Разрешение монитора первое число', name='monitor_resolution1', type=FLOAT),
+            input('Разрешение монитора второе число', name='monitor_resolution2', type=FLOAT),
+            input('Сколько байт для одного пикселя', name='bit', type=FLOAT)
+        ])
+        monitor_resolution1 = data['monitor_resolution1']
+        monitor_resolution2 = data['monitor_resolution2']
         monitor_result = monitor_resolution2 * monitor_resolution1
-        bit = int(input("Сколько байт для одного пикселя "))
+        bit = data['bit']
 
         result = monitor_result * bit / 2 ** 20
         #print(f"Ответ {result} Мбайт")
         put_markdown(f"Ответ {result} Мбайт")
 
-    elif task_type == 3:
-        monitor_resolution1 = int(input("Разрешение изображения 1 число "))
-        monitor_resolution2 = int(input("Разрешение изображения 2 число "))
+    elif task_type == "Узнать максимально возможное число цветов в изображении":
+        data = input_group("Условия задачи", [
+            input('Разрешение монитора первое число', name='monitor_resolution1', type=FLOAT),
+            input('Разрешение монитора второе число', name='monitor_resolution2', type=FLOAT),
+            input('Сколько Кбайт для одного пикселя', name='bit', type=FLOAT)
+        ])
+        monitor_resolution1 = data['monitor_resolution1']
+        monitor_resolution2 = data['monitor_resolution2']
         monitor_result = monitor_resolution2 * monitor_resolution1
-        colors = int(input("Сколько Кбайт для одного пикселя "))
+        colors = data['bit']
         result = colors * 1024 * 8 / monitor_result
         n = 2 ** result
         #print(int(n) + "цветов")
         put_markdown(int(n) + "цветов")
-    else:
-        put_markdown("Не знаю такую:(")
 
 if __name__ == '__main__':
     start_server(main, debug=False, port=8080)
